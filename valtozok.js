d4188c6d1e4211a784ef5d752a6af8dff1a39bbb
d-balcso <deli.balint@outlook.com>
2026-01-15 11:40:17 +0100

    valtozok

---
+++ valtozok.js
@@ -0,0 +1,13 @@
+// VÁLTOZÓ DEKORÁLÁSA   
+console.log("Hello")
+
+var a = 2
+console.log(a);
+
+let b = 3;
+b = 4;
+const c = 5;
+// c = 6; // hiba: konstanst nem tudunk felülírni
+
+//let b = 5; // hiba: ugyanazt a valtozot nem tudjuk ujradekralani
+
